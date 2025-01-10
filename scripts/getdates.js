4b05671e7b0247ef12e90180fb31697cef0317bb
PLD908 <oluyededivine@gmail.com>
2025-01-10 10:42:44 +0100

    updated

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// Use the date object
+const today = new Date();
+
+// Get the current year
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+// Get the last modified date
+const modifiedDate = new Date(document.lastModified);
+lastModified.innerHTML = `<span class="modified">Last Modification: ${new Intl.DateTimeFormat(
+  "en-US",
+  {
+    dateStyle: "short",
+    timeStyle: "short",
+  }
+).format(modifiedDate)}</span>`;
